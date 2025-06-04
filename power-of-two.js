023e7a33f9c24f8ca920689babda42b5d4d03edb
VladaBessikalo <bess.vlada@gmail.com>
2025-06-04 18:03:11 +0200

    feat: bitwise power of two

--- power-of-two.js
+++ power-of-two.js
@@ -1,20 +1,33 @@
+// function isPowerOfTwo(n) {
+//     if (n < 1) {
+//         return false;
+//     }
+
+//     while (n > 1) {
+//         if (n % 2 !== 0) {
+//             return false;
+//         }
+//         n = n / 2;
+//     }
+//     return true;
+// }
+// Big-O = O(log n)
+
+
+// Bitwise Power of two version:
+
 function isPowerOfTwo(n) {
     if (n < 1) {
         return false;
     }
 
-    while (n > 1) {
-        if (n % 2 !== 0) {
-            return false;
-        }
-        n = n / 2;
-    }
-    return true;
-
+    return (n & (n-1)) === 0;
 }
+// Big-O = O(1)
 
 console.log(isPowerOfTwo(1))
 console.log(isPowerOfTwo(2))
 console.log(isPowerOfTwo(5))
 console.log(isPowerOfTwo(8))
 console.log(isPowerOfTwo(10))
+
