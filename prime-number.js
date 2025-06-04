beeec76ff1727adbc49972d9a2dc16cf4c1b04f1
VladaBessikalo <bess.vlada@gmail.com>
2025-06-04 16:58:25 +0200

    refactor: prime number function

--- prime-number.js
+++ prime-number.js
@@ -1,14 +1,24 @@
 // Prime number
 
 function isPrime(n) {
-    if (n < 2) {
-        return false;
+  if (n < 2) {
+    return false;
+  }
+
+  for (let i = 2; i < n; i++) {
+    if (n % i === 0) {
+      return false;
     }
-    const isNatural = n / 2;
-    return !Number.isInteger(isNatural);
+  }
+
+  return true;
 }
 
 console.log(isPrime(1));
 console.log(isPrime(5));
 console.log(isPrime(4));
 console.log(isPrime(0));
+console.log(isPrime(9));
+
+// Big-O = O(n)
+
