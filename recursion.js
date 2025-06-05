fbde163ef8441b6c71ca1532f7de78616badf21d
VladaBessikalo <bess.vlada@gmail.com>
2025-06-05 13:01:10 +0200

    feat: recursion

---
+++ recursion.js
@@ -0,0 +1,11 @@
+function recursiveFibonacci(n) {
+    if (n < 2) {
+        return n;
+    }
+    return recursiveFibonacci(n-1) + recursiveFibonacci(n-2);
+}
+// fibonacci [0, 1, 1, 2, 3, 5, 8, 13, 21 ...]
+console.log(recursiveFibonacci(0)) // 0
+console.log(recursiveFibonacci(1)) // 1
+console.log(recursiveFibonacci(6)) // 8 
+console.log(recursiveFibonacci(7)) // 13
