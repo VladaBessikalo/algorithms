0f6a21945302364a7fe273b2b558af78bb6df3c4
VladaBessikalo <bess.vlada@gmail.com>
2025-06-09 15:56:54 +0200

    refactor: rewrite binary search solution

--- binary-search.js
+++ binary-search.js
@@ -1,36 +1,26 @@
 // Binary search works only for sorted arrays
+ 
 function binarySearch(arr, target) {
-    if (arr.length === 0) {
-        return -1;
-    }
+    let leftIndex = 0;
+    let rightIndex = arr.length - 1;
 
-    const middleElementIndex = Math.floor(arr.length / 2);
-
-    if (target === arr[middleElementIndex]) {
-        return middleElementIndex;
-    }
-
-    if (target > arr[middleElementIndex]) {
-        for (let i = middleElementIndex + 1; i < arr.length; i++) {
-            if (arr[i] === target) {
-                return i;
-            }
+    while (leftIndex <= rightIndex) {
+        let middleIndex = Math.floor((leftIndex + rightIndex) / 2);
+        if (target === arr[middleIndex]) {
+            return middleIndex;
+        }
+        if (target < arr[middleIndex]) {
+            rightIndex = middleIndex - 1
+        } else {
+            leftIndex = middleIndex + 1
         }
     }
-
-        if (target < arr[middleElementIndex]) {
-        for (let i = middleElementIndex - 1; i >= 0; i--) {
-            if (arr[i] === target) {
-                return i;
-            }
-        }
-    }
-
-    return -1;
-
+    return -1
 }
 
 console.log(binarySearch([-5, 2, 3, 4, 5], 5));
 console.log(binarySearch([-5, 2, 3, 4, 5, 7], 4));
 console.log(binarySearch([-5, 2, 4, 5, 10], 120));
 console.log(binarySearch([-4, 2, 4, 5, 10, 15], 2));
+
+// Big O = O(log n) - logarithmic time complexity
