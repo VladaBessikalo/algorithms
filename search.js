9ab8cb46637773de5c6fc2ef9ac74c01ee98b94b
VladaBessikalo <bess.vlada@gmail.com>
2025-06-09 14:49:53 +0200

    feat: add another linear search solution

--- search.js
+++ search.js
@@ -1,11 +1,22 @@
-function linearSearch(arr, n) {
+// my solution
+// function linearSearch(arr, n) {
+//     for (let i = 0; i < arr.length; i++) {
+//         if (arr.includes(n)) {
+//             return arr.indexOf(n);
+//         } else {
+//             return -1;
+//         }
+//     }
+// }
+
+// basic solution
+function linearSearch(arr, target) {
     for (let i = 0; i < arr.length; i++) {
-        if (arr.includes(n)) {
-            return arr.indexOf(n);
-        } else {
-            return -1;
+        if (arr[i] === target) {
+            return i;
         }
     }
+    return -1;
 }
 
 
@@ -14,3 +25,5 @@ console.log(linearSearch([-5, 2, 10, 4, 5], 10));
 console.log(linearSearch([-5, 2, 10, 4, 5], 4));
 console.log(linearSearch([-5, 2, 10, 4, 5], 120));
 console.log(linearSearch([4, 2, 10, 4, 5], 4));
+
+// Big-O - O(n) linear time complexity
