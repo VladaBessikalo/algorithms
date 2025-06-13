8cb61ba070f3cffb3ee61f30e904224a89119f71
VladaBessikalo <bess.vlada@gmail.com>
2025-06-13 13:47:26 +0200

    fix: add info about time complexity

--- data-structures/arrays.js
+++ data-structures/arrays.js
@@ -2,3 +2,12 @@ const arr = [1, 2, 3, 'Vlada'];
 arr.unshift(0)
 
 console.log(arr);
+
+// Big-O:
+// Insert/remove from end O(1)
+// Insert / remove from beginning - O(n) - we need to reassign indexes to each element
+// Access - O(1)
+// Search - O(n)
+// Push / pop - O(1)
+// Shift, unshift, concat, slice, splice - O(n)
+// forEach, map, filter, reduce - O(n)
--- data-structures/objects.js
+++ data-structures/objects.js
@@ -1,8 +1,26 @@
 const obj = {
-    name: "Vlada",
-    age: 31
+    name: "Vlada B",
+    age: 31,
+    'key': true,
+    sayMyName: function() {
+        console.log("function", this.name);
+        
+    }
 }
+
+obj.hobby = "running"
+delete obj.key
 
 console.log(obj.name);
 console.log(obj['age']);
+console.log(obj.key);
+console.log(obj['key']);
+console.log(obj);
+obj.sayMyName();
+
+// Object.keys(), Object.values(), Object.entries() - O(n)
+
+// Big-O - insert - O(1), remove - O(1), Access - O(1), Search - O(n)
+
+
 
