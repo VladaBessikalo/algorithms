f0f8c004216d5ccae0d2238fd48774f5ebfade57
VladaBessikalo <bess.vlada@gmail.com>
2025-06-13 13:34:48 +0200

    feat: obj and arr structure

---
+++ data-structures/arrays.js
@@ -0,0 +1,4 @@
+const arr = [1, 2, 3, 'Vlada'];
+arr.unshift(0)
+
+console.log(arr);
---
+++ data-structures/objects.js
@@ -0,0 +1,8 @@
+const obj = {
+    name: "Vlada",
+    age: 31
+}
+
+console.log(obj.name);
+console.log(obj['age']);
+
